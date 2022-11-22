0bed87098befe40f423b5c74e4d671c781d01164
yeargun <yeargun24@gmail.com>
2022-11-22 15:59:14 +0300

    beautify some9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Layout from './components/Layout'
-import './App.scss'
-import { Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Portfolio from './components/Portfolio'
-import DinoGame from './components/DinoGame'
+import Layout from "./components/Layout";
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+import DinoGame from "./components/DinoGame";
 // import Dashboard from "./components/Dashboard"
 
 function App() {
@@ -17,11 +17,10 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
